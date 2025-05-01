0f9ae36138fc75be13340637f2461da4463e506c
Akshit Yadav <akshit07032001@gmail.com>
2025-05-01 21:08:40 +0530

    favorites

--- src/context/GifyContext.jsx
+++ src/context/GifyContext.jsx
@@ -1,5 +1,6 @@
 import { GiphyFetch } from '@giphy/js-fetch-api';
 import { createContext } from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import React from 'react';
 import { useContext } from 'react';
@@ -13,9 +14,38 @@ const GifyContextProvider = ({ children }) => {
 
   const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_KEY);
 
+  const addToFavorites = (id) => {
+    if (favorites.includes(id)) {
+      const updatedFavorites = favorites.filter((itemId) => itemId !== id);
+      localStorage.setItem('favoriteGIFs', JSON.stringify(updatedFavorites));
+      setFavorites(updatedFavorites);
+    } else {
+      const updatedFavorites = [...favorites];
+      updatedFavorites.push(id);
+      localStorage.setItem('favoriteGIFs', JSON.stringify(updatedFavorites));
+      setFavorites(updatedFavorites);
+    }
+  };
+
+  useEffect(() => {
+    const favoritesFromLS =
+      JSON.parse(localStorage.getItem('favoriteGIFs')) || [];
+
+    setFavorites(favoritesFromLS);
+  }, []);
+
   return (
     <GifyContext.Provider
-      value={{ gf, gifs, setGifs, favorites, setFavorites, filter, setFilter }}
+      value={{
+        gf,
+        gifs,
+        setGifs,
+        favorites,
+        setFavorites,
+        filter,
+        setFilter,
+        addToFavorites,
+      }}
     >
       {children}
     </GifyContext.Provider>
