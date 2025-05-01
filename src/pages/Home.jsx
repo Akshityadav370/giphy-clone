bc5bd77c4d22c116599137a54a1af531f3603b6a
Akshit Yadav <akshit07032001@gmail.com>
2025-05-01 21:01:27 +0530

    gif n filter gif

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { GifyContextState } from '../context/GifyContext';
+import { useEffect } from 'react';
+import FilterGif from '../components/FilterGif';
+import Gif from '../components/Gif';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { gf, gifs, filter, setGifs } = GifyContextState();
+
+  const fetchTrendingGifs = async () => {
+    const { data } = await gf.trending({
+      limit: 20,
+      type: filter,
+      rating: 'g',
+    });
+    setGifs(data);
+  };
+
+  useEffect(() => {
+    fetchTrendingGifs();
+  }, [filter]);
+
+  return (
+    <div>
+      <img
+        src='/banner.gif'
+        alt='earth banner'
+        className='mt-2 rounded w-full'
+      />
+
+      <FilterGif showTrending />
+      <div className='columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2'>
+        {gifs.map((gif) => (
+          <Gif gif={gif} key={gif.title} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
